46f175c68f8ecba7c12c3dca3de86261d7d42ab5
Leandre354 <leandre.cuenot@students.unibe.ch>
2024-10-09 11:22:12 +0200

    Update with second observer

--- Analysis/clinical_plot.py
+++ Analysis/clinical_plot.py
@@ -22,6 +22,8 @@ def autopct_format(values):
 db = pd.read_csv('Inference/Image200/Analysis/Baseline/Metrics_x_Clinical.csv', delimiter=';')
 db2 = pd.read_csv('Inference/Image200/Analysis/Baseline/Metrics_x_Clinical_perturbed.csv', delimiter=';')
 
+observer = 'Likert 2'
+
 dice_bins = [0, 0.20, 0.40, 0.60, 0.80, 1]
 dice_labels = ['0-0.20', '0.20-0.40', '0.40-0.60', '0.60-0.80', '0.80-1']
 
@@ -60,10 +62,10 @@ plt.pie(counts_list_dice3, labels=dice_labels, autopct=autopct_format(counts_lis
 plt.subplot(2, 2, 4).set_title('Primary perturbed DICE')
 plt.pie(counts_list_dice4, labels=dice_labels, autopct=autopct_format(counts_list_dice4), textprops={'fontsize': 7})
 
-counts = Counter(db['Likert'].iloc[::2])
-counts2 = Counter(db['Likert'].iloc[1::2])
-counts3 = Counter(db2['Likert'].iloc[::2])
-counts4 = Counter(db2['Likert'].iloc[1::2])
+counts = Counter(db[observer].iloc[::2])
+counts2 = Counter(db[observer].iloc[1::2])
+counts3 = Counter(db2[observer].iloc[::2])
+counts4 = Counter(db2[observer].iloc[1::2])
 
 # Create lists of ratings and their corresponding counts
 ratings = [1, 2, 3, 4, 5]
@@ -87,60 +89,100 @@ plt.pie(counts_list4, labels=ratings, autopct=autopct_format(counts_list4), text
 plt.figure('Correlation')
 
 plt.subplot(2,3,1).set_title('Nodal Likert v Dice')
-plt.scatter(db['Likert'].iloc[::2], db['DICE'].iloc[::2], marker='.')
-m, b = np.polyfit(db['Likert'].iloc[::2], db['DICE'].iloc[::2], 1)
-plt.plot(db['Likert'].iloc[::2], m * db['Likert'].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(db['Likert'].iloc[::2], db['DICE'].iloc[::2])
+plt.scatter(db[observer].iloc[::2], db['DICE'].iloc[::2], marker='.')
+m, b = np.polyfit(db[observer].iloc[::2], db['DICE'].iloc[::2], 1)
+plt.plot(db[observer].iloc[::2], m * db[observer].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db[observer].iloc[::2], db['DICE'].iloc[::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
 
 plt.subplot(2,3,2).set_title('Nodal Likert v Hausdorff')
 filtered_db = db[db['HDRFDST'] != 1000]
-plt.scatter(filtered_db['Likert'].iloc[::2], filtered_db['HDRFDST'].iloc[::2], marker='.')
-m, b = np.polyfit(filtered_db['Likert'].iloc[::2], filtered_db['HDRFDST'].iloc[::2], 1)
-plt.plot(filtered_db['Likert'].iloc[::2], m * filtered_db['Likert'].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(filtered_db['Likert'].iloc[::2], filtered_db['HDRFDST'].iloc[::2])
+plt.scatter(filtered_db[observer].iloc[::2], filtered_db['HDRFDST'].iloc[::2], marker='.')
+m, b = np.polyfit(filtered_db[observer].iloc[::2], filtered_db['HDRFDST'].iloc[::2], 1)
+plt.plot(filtered_db[observer].iloc[::2], m * filtered_db[observer].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(filtered_db[observer].iloc[::2], filtered_db['HDRFDST'].iloc[::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
 
 plt.subplot(2,3,3).set_title('Nodal Likert v Jaccard')
-plt.scatter(db['Likert'].iloc[::2], db['JACRD'].iloc[::2], marker='.')
-m, b = np.polyfit(db['Likert'].iloc[::2], db['JACRD'].iloc[::2], 1)
-plt.plot(db['Likert'].iloc[::2], m * db['Likert'].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(db['Likert'].iloc[::2], db['JACRD'].iloc[::2])
+plt.scatter(db[observer].iloc[::2], db['JACRD'].iloc[::2], marker='.')
+m, b = np.polyfit(db[observer].iloc[::2], db['JACRD'].iloc[::2], 1)
+plt.plot(db[observer].iloc[::2], m * db[observer].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db[observer].iloc[::2], db['JACRD'].iloc[::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
 
 plt.subplot(2,3,4).set_title('Primary Likert v Dice')
-plt.scatter(db['Likert'].iloc[1::2], db['DICE'].iloc[1::2], marker='.')
-m, b = np.polyfit(db['Likert'].iloc[1::2], db['DICE'].iloc[1::2], 1)
-plt.plot(db['Likert'].iloc[1::2], m * db['Likert'].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(db['Likert'].iloc[1::2], db['DICE'].iloc[1::2])
+plt.scatter(db[observer].iloc[1::2], db['DICE'].iloc[1::2], marker='.')
+m, b = np.polyfit(db[observer].iloc[1::2], db['DICE'].iloc[1::2], 1)
+plt.plot(db[observer].iloc[1::2], m * db[observer].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db[observer].iloc[1::2], db['DICE'].iloc[1::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
 
 plt.subplot(2,3,5).set_title('Primary Likert v Hausdorff')
 filtered_db = db[db['HDRFDST'] != 1000]
-plt.scatter(filtered_db['Likert'].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2], marker='.')
-m, b = np.polyfit(filtered_db['Likert'].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2], 1)
-plt.plot(filtered_db['Likert'].iloc[1::2], m * filtered_db['Likert'].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(filtered_db['Likert'].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2])
+plt.scatter(filtered_db[observer].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2], marker='.')
+m, b = np.polyfit(filtered_db[observer].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2], 1)
+plt.plot(filtered_db[observer].iloc[1::2], m * filtered_db[observer].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(filtered_db[observer].iloc[1::2], filtered_db['HDRFDST'].iloc[1::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
 
 plt.subplot(2,3,6).set_title('Primary Likert v Jaccard')
-plt.scatter(db['Likert'].iloc[1::2], db['JACRD'].iloc[1::2], marker='.')
-m, b = np.polyfit(db['Likert'].iloc[1::2], db['JACRD'].iloc[1::2], 1)
-plt.plot(db['Likert'].iloc[1::2], m * db['Likert'].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
-corr, p = stats.pearsonr(db['Likert'].iloc[1::2], db['JACRD'].iloc[1::2])
+plt.scatter(db[observer].iloc[1::2], db['JACRD'].iloc[1::2], marker='.')
+m, b = np.polyfit(db[observer].iloc[1::2], db['JACRD'].iloc[1::2], 1)
+plt.plot(db[observer].iloc[1::2], m * db[observer].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db[observer].iloc[1::2], db['JACRD'].iloc[1::2])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
+
+
+plt.figure('Physician correlation')
+plt.subplot(2,2,1).set_title('Nodal Observer 1 v Observer 2')
+plt.scatter(db['Likert 1'].iloc[::2], db['Likert 2'].iloc[::2], marker='.')
+m, b = np.polyfit(db['Likert 1'].iloc[::2], db['Likert 2'].iloc[::2], 1)
+plt.plot(db['Likert 1'].iloc[::2], m * db['Likert 1'].iloc[::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db['Likert 1'].iloc[::2], db['Likert 2'].iloc[::2])
+xlim = plt.xlim(0.8, 5.2)
+ylim = plt.ylim(0.8, 5.2)
+plt.xlabel('Observer 1 Ratings')
+plt.ylabel('Observer 2 Ratings')
+plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
+
+plt.subplot(2, 2, 2).set_title('Heatmap of Rating Agreement')
+heatmap_data = pd.crosstab(db['Likert 2'].iloc[::2], db['Likert 1'].iloc[::2])  # Create a crosstab for the heatmap
+sns.heatmap(heatmap_data, annot=True, cmap='Reds', cbar=True, fmt='d')
+plt.xlabel('Observer 1 Ratings')
+plt.ylabel('Observer 2 Ratings')
+plt.gca().invert_yaxis()  # Inverts the Y-axis
+
+plt.subplot(2,2,3).set_title('Primary Observer 1 v Observer 2')
+plt.scatter(db['Likert 1'].iloc[1::2], db['Likert 2'].iloc[1::2], marker='.')
+m, b = np.polyfit(db['Likert 1'].iloc[1::2], db['Likert 2'].iloc[1::2], 1)
+plt.plot(db['Likert 1'].iloc[1::2], m * db['Likert 1'].iloc[1::2] + b, color='red', label=f'Trendline: y={m:.2f}x+{b:.2f}')
+corr, p = stats.pearsonr(db['Likert 1'].iloc[1::2], db['Likert 2'].iloc[1::2])
+xlim = plt.xlim(0.8, 5.2)
+ylim = plt.ylim(0.8, 5.2)
+plt.xlabel('Observer 1 Ratings')
+plt.ylabel('Observer 2 Ratings')
+plt.text(xlim[1] - 0.2 * (xlim[1] - xlim[0]), ylim[1] - 0.1 * (ylim[1] - ylim[0]),f'{corr:.3f}')
+
+plt.subplot(2, 2, 4).set_title('Heatmap of Rating Agreement')
+heatmap_data = pd.crosstab(db['Likert 2'].iloc[1::2], db['Likert 1'].iloc[1::2]).reindex(index=range(1, 6), columns=range(1, 6), fill_value=0)  # Create a crosstab for the heatmap
+sns.heatmap(heatmap_data, annot=True, cmap='Reds', cbar=True, fmt='d')
+plt.xlabel('Observer 1 Ratings')
+plt.ylabel('Observer 2 Ratings')
+plt.tight_layout()
+
+plt.gca().invert_yaxis()  # Inverts the Y-axis
 
 # Show the plot
 plt.show()
